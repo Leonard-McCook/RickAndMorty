f1ab6deceb3ee5bcae4f207ea73a6be5ade0632f
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-08-06 20:56:47 -0400

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Leonard McCook-Carr on 8/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
