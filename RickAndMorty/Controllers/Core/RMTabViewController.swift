8f9293476ec1e04e309bbd6b608863d576c87c8a
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-08-09 02:15:58 -0400

    Attach controllers to tab bar

--- RickAndMorty/Controllers/Core/RMTabViewController.swift
+++ RickAndMorty/Controllers/Core/RMTabViewController.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 
-class RMTabBarController: UITabBarController {
+final class RMTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setUpTabs()
     }
 
-
+    private func setUpTabs() {
+        let charactersVC = RMCharacterViewController()
+        let locationsVC = RMLocationViewController()
+        let episodesVC = RMEpisodeViewController()
+        let settingsVC = RMSettingsViewController()
+        
+        charactersVC.navigationItem.largeTitleDisplayMode = .automatic
+        locationsVC.navigationItem.largeTitleDisplayMode = .automatic
+        episodesVC.navigationItem.largeTitleDisplayMode = .automatic
+        settingsVC.navigationItem.largeTitleDisplayMode = .automatic
+        
+        let nav1 = UINavigationController(rootViewController: charactersVC)
+        let nav2 = UINavigationController(rootViewController: locationsVC)
+        let nav3 = UINavigationController(rootViewController: episodesVC)
+        let nav4 = UINavigationController(rootViewController: settingsVC)
+        
+        for nav in  [nav1, nav2, nav3, nav4] {
+            nav.navigationBar.prefersLargeTitles = true
+        }
+        
+        setViewControllers(
+            [nav1, nav2, nav3, nav4],
+            animated: true
+        )
+    }
 }
 
