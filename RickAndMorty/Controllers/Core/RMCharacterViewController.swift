3949dddaf1832e2a336d7eef955943642c1dff1b
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-08-10 15:28:44 -0400

    Add icons to tab bar menu

--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -7,23 +7,21 @@
 
 import UIKit
 
-class RMCharacterViewController: UIViewController {
+final class RMCharacterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemBackground
+        title = "Characters"
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+
+//
+//charactersVC.title = "Characters"
+//locationsVC.title = "Locations"
+//episodesVC.title = "Episodes"
+//settingsVC.title = "Settings"
+
