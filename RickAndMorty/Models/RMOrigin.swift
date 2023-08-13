9463265c352bf0e7491709824292820b6e9bbe09
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-08-13 04:55:12 -0400

    Create RMOrigin and RMSingleLocation files in the Models folder

---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,14 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Leonard McCook-Carr on 8/13/23.
+//
+
+import Foundation
+
+
+struct RMOrigin: Codable {
+   let name: String
+   let url: String
+}
---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,13 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by Leonard McCook-Carr on 8/13/23.
+//
+
+import Foundation
+
+struct RMSingleLocation: Codable {
+    let name: String
+    let url: String
+}
