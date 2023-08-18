d0020fdf25aa0d26734760b08f15e6d2a6d9a036
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-08-18 15:07:57 -0400

    Create Extensions file in the Resources folder and CharacterListView file in the Views folder

--- RickAndMorty/ViewModels/CharacterListViewViewModel.swift
+++ RickAndMorty/ViewModels/CharacterListViewViewModel.swift
@@ -5,9 +5,9 @@
 //  Created by Leonard McCook-Carr on 8/17/23.
 //
 
-import Foundation
+import UIKit
 
-struct CharacterListViewViewModel {
+final class CharacterListViewViewModel: NSObject {
     func fetchCharacters() {
         RMService.shared.execute(.listCharactersRequests, expecting: RMGetAllCharactersResponse.self) { result in
             switch result {
@@ -18,5 +18,26 @@ struct CharacterListViewViewModel {
                 print(String(describing: error))
             }
         }
+    }
+}
+
+extension CharacterListViewViewModel: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemGreen
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        let bounds = UIScreen.main.bounds
+        let width = (bounds.width-30)/2
+        return CGSize(
+            width: width,
+            height: width * 1.5
+        )
     }
 }
