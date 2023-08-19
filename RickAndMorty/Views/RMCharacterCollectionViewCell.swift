fc218032294c35a84037347b7a33a6d9e5b3df73
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-08-19 00:39:39 -0400

    Create RMCharacterCollectionViewCell file in the Views folder

---
+++ RickAndMorty/Views/RMCharacterCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RMCharacterCollectionViewCell.swift
+//  RickAndMorty
+//
+//  Created by Leonard McCook-Carr on 8/19/23.
+//
+
+import UIKit
+
+/// Single cell for a character
+final class RMCharacterCollectionViewCell: UICollectionViewCell {
+    static let cellIdentifier = "RMCharacterCollectionViewCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.backgroundColor = .secondarySystemBackground
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("Unsupported")
+        }
+        
+    }
+
