52264190a1f52481e3fe346187277f1604b86d8a
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-08-22 08:15:15 -0400

    Create Other subfolder and add RMCharacterDetailViewController file

---
+++ RickAndMorty/Controllers/Other/RMCharacterDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RMCharacterDetailViewController.swift
+//  RickAndMorty
+//
+//  Created by Leonard McCook-Carr on 8/22/23.
+//
+
+import UIKit
+
+/// Controller to show info about single character
+final class RMCharacterDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
