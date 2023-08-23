5ec4a2ea10a7731bbe1d7531e5173bd480afb322
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-08-23 15:06:01 -0400

    Create RMCharacterDetailView and RMCharacterDetailViewViewModel files

---
+++ RickAndMorty/Views/RMCharacterDetailView.swift
@@ -0,0 +1,13 @@
+//
+//  RMCharacterDetailView.swift
+//  RickAndMorty
+//
+//  Created by Leonard McCook-Carr on 8/23/23.
+//
+
+import UIKit
+
+final class RMCharacterDetailView: UIView {
+
+    
+}
