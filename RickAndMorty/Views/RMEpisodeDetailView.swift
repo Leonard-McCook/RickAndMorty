091078fbff55b2d7a2053bb1a053fdaa5a39697b
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-09-06 16:48:58 -0400

    Setup API Cache Layer

---
+++ RickAndMorty/Views/RMEpisodeDetailView.swift
@@ -0,0 +1,19 @@
+//
+//  RMEpisodeDetailView.swift
+//  RickAndMorty
+//
+//  Created by Leonard McCook-Carr on 9/6/23.
+//
+
+import UIKit
+
+class RMEpisodeDetailView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Unsupported")
+    }
+}
