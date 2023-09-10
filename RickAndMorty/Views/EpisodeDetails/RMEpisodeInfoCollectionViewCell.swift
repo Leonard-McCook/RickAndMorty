7507cfba9c47450705b99be31de238fb08fb8f78
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-09-10 05:04:41 -0400

    Create EpisodeDetails folder in the Views Group and add EpisodeInfoCollectionViewCell file

---
+++ RickAndMorty/Views/EpisodeDetails/RMEpisodeInfoCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RMEpisodeInfoCollectionViewCell.swift
+//  RickAndMorty
+//
+//  Created by Leonard McCook-Carr on 9/10/23.
+//
+
+import UIKit
+
+final class RMEpisodeInfoCollectionViewCell: UICollectionViewCell {
+    
+}
