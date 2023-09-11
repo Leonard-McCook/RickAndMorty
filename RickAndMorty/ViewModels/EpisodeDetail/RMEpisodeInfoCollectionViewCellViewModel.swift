7a940e404512d6c6ca3db2040755220bfe8b7887
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-09-11 17:27:16 -0400

    Set up Episode Detail Layout

---
+++ RickAndMorty/ViewModels/EpisodeDetail/RMEpisodeInfoCollectionViewCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  RMEpisodeInfoCollectionViewCellViewModel.swift
+//  RickAndMorty
+//
+//  Created by Leonard McCook-Carr on 9/11/23.
+//
+
+import Foundation
+
+struct RMEpisodeCollectionCellViewModel {
+    public let title: String
+    public let value: String
+}
