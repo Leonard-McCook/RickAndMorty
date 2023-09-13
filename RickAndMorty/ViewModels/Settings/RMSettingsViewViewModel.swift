b7211bb18e883ccbad151b562cf0ef87e90af4b2
Leonard-McCook <leonard.mccookcarr@outlook.com>
2023-09-13 12:16:23 -0400

    Create Settings folder and add files

---
+++ RickAndMorty/ViewModels/Settings/RMSettingsViewViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  RMSettingsViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Leonard McCook-Carr on 9/13/23.
+//
+
+import Foundation
+
+struct RMSettingsViewViewModel {
+    let cellViewModels: [RMSettingsCellViewModel]
+}
+
